4da5f21116ec4cbf5146387b62737600eeb80d6a
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-10-09 17:13:55 +0530

    Diffing two trees

--- todo-app.js
+++ todo-app.js
@@ -24,4 +24,20 @@ console.log(Step1);
 // Step 5, 6
 TinyReact.render(Step1, root);
 
+var Step2 = (
+  <div>
+    <h1 className="header">Hello Tiny React!</h1>
+    <h2>(coding nirvana)</h2>
+    <div>nested 1<div>nested 1.1</div></div>
+    <h3>(OBSERVE: I said it!!)</h3>
+    {2 == 1 && <div>Render this if 2==1</div>}
+    {2 == 2 && <div>{2}</div>}
+    <span>Something goes here...</span>
+    <button onClick={() => alert("Hi!")}>Click me!</button>
+  </div>
+);
 
+setTimeout(() => {
+  alert("Re-rendering...");
+  TinyReact.render(Step2, root);
+}, 4000);
