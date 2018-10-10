cb67dce66b5f404e60cf3f2d022803dc5de6ebb3
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-10-10 09:14:49 +0530

    CSS changes and testing diffing

--- todo-app.js
+++ todo-app.js
@@ -29,11 +29,11 @@ var Step2 = (
     <h1 className="header">Hello Tiny React!</h1>
     <h2>(coding nirvana)</h2>
     <div>nested 1<div>nested 1.1</div></div>
-    <h3>(OBSERVE: I said it!!)</h3>
+    <h3 style="background-color:yellow">(OBSERVE: I said it!!)</h3>
     {2 == 1 && <div>Render this if 2==1</div>}
     {2 == 2 && <div>{2}</div>}
     <span>Something goes here...</span>
-    <button onClick={() => alert("Hi!")}>Click me!</button>
+    <button onClick={() => alert("This has changed!")}>Click me!</button>
   </div>
 );
 
